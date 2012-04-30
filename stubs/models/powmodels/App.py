5b15000a2ba21ca4d7780791192c78106bf9f7cc
khz <khz@tzi.org>
2012-05-01 00:56:34 +0200

    corrected some import bugs in App.py and Relation.py

--- stubs/models/powmodels/App.py
+++ stubs/models/powmodels/App.py
@@ -1,6 +1,6 @@
 import sys,os
 
-sys.path.append( os.path.abspath(os.path.join( os.path.dirname(os.path.abspath(__file__)), "../lib" )))
+sys.path.append( os.path.abspath(os.path.join( os.path.dirname(os.path.abspath(__file__)), "../../lib" )))
 sys.path.append( os.path.abspath(os.path.join( os.path.dirname(os.path.abspath(__file__)), "../basemodels" )))
 sys.path.append( os.path.abspath(os.path.join( os.path.dirname(os.path.abspath(__file__)), "../basemodels/pow" )))
 
