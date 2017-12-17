4c7a1f13ef148b4e1e620a06e3c2b9a8c29b062c
khz <khz@pythononwheels.org>
2017-12-17 20:13:44 +0100

    new Version 0.7b

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 
 setup(name='pythononwheels',
-      version='0.6.22b',
+      version='0.7.0b',
       description='The simple, quick and easy generative web framework for python',
       long_description=long_description,
       classifiers=[
