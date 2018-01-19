e1bac64b4d67d7a1d89613bed18acda2c6076971
khz <khz@pythononwheels.org>
2018-01-19 21:28:36 +0100

    renamed the semanticui stubs to .sui

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 
 setup(name='pythononwheels',
-      version='0.81b',
+      version='0.812b',
       description='The simple, quick and easy generative web framework for python',
       long_description=long_description,
       classifiers=[
