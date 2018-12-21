7d697fddfaa6669b8fa13dee5dcedc22c17efaf7
pythononwheels <info@pythononwheels.org>
2018-12-21 16:03:50 +0100

    updated the shorties handlers. Better order. Deleted redundancies

--- pythononwheels/start/handlers/shorties.py
+++ pythononwheels/start/handlers/shorties.py
@@ -3,29 +3,30 @@ import tornado.web
 from {{appname}}.handlers.base import BaseHandler
 from {{appname}}.application import app
 
-# you can use regex in the routes as well:
+# You can uese flask/werkzeug routing.
+# @app.add_route('/test/<uuid:identifier>', dispatch={"get" : "testuuid"})
+# Or you can use regex in the routes as well:
 # (r"/([^/]+)/(.+)", ObjectHandler),
 # any regex goes. any group () will be handed to the handler 
-# 
+# see example handler below.
 
-# if you specify a method, this method will be called for this route
-@app.add_route("/thanks/*", dispatch={"get": "_get"} )
-@app.add_route("/thanks/([0-9]+)*", dispatch={"get": "testme"})
-class ThanksHandler(BaseHandler):
-    def _get(self):
-        print("  .. in _get" )
-        self.render("thanks.tmpl")
 
-    def testme(self, index=0 ):
-        print("  .. in testme: index = " + str(index))
-        self.render("thanks.tmpl", index=index)
-    
-# if you DON't specify a method, the standard HTTP verb method (e.g. get(), put() will be called)
-@app.add_route("/index/([0-9]+)*")
+
 @app.add_route("/", pos=1)
+@app.add_route('/index/<int:year>', dispatch={"get" : "test"})
+#@app.add_route('/index/<uuid:identifier>', dispatch={"get" : "testuuid"})
+#@app.add_route('/index/<uuid:identifier>.<format>', dispatch={"get" : "testuuid"})
 class IndexdHandler(BaseHandler):
-    def get(self, index=None):
-        print(" Calling IndexHandler from handlers/shorties.py: parameter index: " + str(index))
+    def get(self, year=None):
+        print(" Calling IndexHandler from handlers/shorties.py: parameter index: " + str(year))
+        self.render("index.tmpl")
+    
+    def get(self, year=None):
+        print(" Calling IndexHandler from handlers/shorties.py: parameter index: " + str(year))
+        self.render("index.tmpl")
+    
+    def testuuid(self, identifier=None, format=None):
+        print(" Calling Indexhandler. Indentifier: {}, format: {}".format(str(identifier), str(format)))
         self.render("index.tmpl")
 
 # this will be the last route since it has the lowest pos.
@@ -34,42 +35,4 @@ class ErrorHandler(BaseHandler):
     def get(self):
         return self.error( template="404.tmpl", http_code=404  )
 
-@app.add_route("/test/([0-9]+)*", dispatch={"get" : "test"})
-@app.add_route("/testresults", dispatch={"get" : "_show_results"})
-class TestHandler(BaseHandler):
-    # on HTTP GET this method will be called. See dispatch parameter.
-    def test(self, index=None):
-        self.write(index)
-    
-    def _show_results(self):
-        """
-            this action will show the testresults from
-            test/runtests.py 
-        """
-        self.render("result.html")
-    
-@app.add_rest_routes("rest")
-class RestHandler(BaseHandler):
-    # on HTTP GET this method will be called. See config.py "default_rest_route"
-    def list(self):
-        self.write("REST")
 
-
-@app.add_route('/werkzeug/<int:year>', dispatch={"get" : "test"})
-@app.add_route('/werkzeug/<uuid:identifier>', dispatch={"get" : "testuuid"})
-@app.add_route('/werkzeug/<uuid:identifier>.<format>', dispatch={"get" : "testuuid"})
-class WerkzeugTestHandler(BaseHandler):
-    # on HTTP GET this method will be called. See dispatch parameter.
-    def test(self, year=None):
-        self.write("I got year: " + str(year))
-    
-    def testuuid(self, anotherone=None, identifier=None, format="html"):
-        self.write("I got uuid: " + str(identifier)
-                    + "<hr> I got anotherone ? == " + str(anotherone)
-        )
-
-@app.add_route("/errortest", dispatch={"get" : "errortest"})
-class ErrorTestHandler(BaseHandler):
-    # on HTTP GET this method will be called. See dispatch parameter.
-    def errortest(self):
-        self.error(message="ERRORTEST", data=[])
