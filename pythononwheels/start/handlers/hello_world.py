8bcb6c3b59c05b7c6c7f12125523bea33e8876f9
pythononwheels <khz@tzi.org>
2019-04-19 00:22:18 +0100

    added PowHandler as baseclass for HelloWorld handler as well

--- pythononwheels/start/handlers/hello_world.py
+++ pythononwheels/start/handlers/hello_world.py
@@ -1,10 +1,10 @@
 import tornado.ioloop
 import tornado.web
-from {{appname}}.handlers.base import BaseHandler
+from {{appname}}.handlers.powhandler import PowHandler
 from {{appname}}.application import app, route
 
 @app.make_routes()
-class HelloHandler(BaseHandler):
+class HelloHandler(PowHandler):
     @route(r'/hello', dispatch=["get"])
     def hello(self):
         self.write("Hello world!")
