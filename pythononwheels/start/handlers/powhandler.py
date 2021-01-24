90a0033e4c67a18d6d43cc437ca658069218903e
pythononwheels <pythononwheels@pythononwheels.org>
2021-01-24 11:29:35 +0100

    start adding websockets

--- pythononwheels/start/handlers/powhandler.py
+++ pythononwheels/start/handlers/powhandler.py
@@ -5,8 +5,9 @@ import werkzeug.security
 import os
 from {{appname}}.conf.config import myapp 
 from {{appname}}.handlers.base import BaseHandler
+from {{appname}}.handlers.powhandlermixin import PowHandlerMixin
 
-class PowHandler(BaseHandler):
+class PowHandler(PowHandlerMixin, BaseHandler):
     """
         The Base PowHandler 
         Place to put common stuff which will remain unaffected by any PoW Changes.
@@ -14,45 +15,7 @@ class PowHandler(BaseHandler):
 
         Example is authentication.
     """
-    show_list=[]
-    hide_list=[]
-    def get_current_user(self):
-        """
-            very simple implementation. 
-            change to you own needs here or in your own subclassed base handler.
-
-        """
-        if myapp["enable_auth"]:
-            # try to find the user
-            # user_id = self.get_secure_cookie("user_id")
-            # if not user_id: return None
-            # u=User()
-            # u=u.find_one(User.id==user_id)
-            # return u
-            raise NotImplementedError("User Authentication not implemented, yet")
-        else:
-            # if authentication is disabled return a dummy guest user
-            return True
-        
-    def check_password_hash(self, pwhash, password ):
-        """
-            uses werkzeug.security.check_password_hash
-            see: http://werkzeug.pocoo.org/docs/0.14/utils/#module-werkzeug.security
-            get the password from for example a login form (make sure you use https)
-            get the hash from the user model table (see generate_password_hash below)
-        """
-        return werkzeug.security.check_password_hash(pwhash, password)
-
-    def generate_password_hash(self, password ):
-        """
-            uses werkzeug.security.generate_password_hash 
-            see: http://werkzeug.pocoo.org/docs/0.14/utils/#module-werkzeug.security
-            store this returned hash in the user models table as password
-            when the user is first registered or changed his password.
-            Use https to secure the plaintext POSTed pwd.
-        """
-        method = myapp["pwhash_method"]
-        return werkzeug.security.generate_password_hash(password, method=method, salt_length=8)
+   
 
     def get_post_file(self, body=False, form_field_name="file" ):
         """ 
